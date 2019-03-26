2dafb303159898c7e8ddf40d9fb41ea0d103a355
ag <toarvind.gupta@gmail.com>
2019-03-26 10:59:58 +0530

    first commit project

--- app/src/main/java/com/example/m1037344/myapplication/MainActivity.java
+++ app/src/main/java/com/example/m1037344/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"hello world 123",Toast.LENGTH_SHORT).show();
+        System.out.println("Test project :");
     }
 }
